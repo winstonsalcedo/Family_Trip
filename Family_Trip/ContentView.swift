09deb2ca71b3037ac98b5d9e5417362395f8369e
Winston Salcedo <winstonsalcedo@gmail.com>
2019-08-03 15:09:15 -0600

    initial commit

---
+++ Family_Trip/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Family_Trip
+//
+//  Created by winston salcedo on 8/3/19.
+//  Copyright © 2019 Evolve Technologies. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ Family_TripTests/Family_TripTests.swift
@@ -0,0 +1,34 @@
+//
+//  Family_TripTests.swift
+//  Family_TripTests
+//
+//  Created by winston salcedo on 8/3/19.
+//  Copyright © 2019 Evolve Technologies. All rights reserved.
+//
+
+import XCTest
+@testable import Family_Trip
+
+class Family_TripTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
